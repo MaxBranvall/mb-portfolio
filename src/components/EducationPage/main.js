9381b393328dc573a111e3f2a4de75b684195ccd
Maxwell J Branvall <36718512+MaxBranvall@users.noreply.github.com>
2021-09-26 10:56:02 -0400

    - Got started on education section

--- src/components/EducationPage/main.js
+++ src/components/EducationPage/main.js
@@ -1,13 +1,29 @@
 
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Icon, Flex, Text } from "@chakra-ui/react";
+import { IoSchool } from 'react-icons/io5';
 
 function EducationPage() {
 
     return (
 
-        <Box h="100vh" bg="red.400" textAlign="center">
+        <Box h="100vh" textAlign="center" background="linear-gradient(to left bottom, #2f3441 50%, #212531 50%)">
 
             <Heading variant="mainHeader">Education</Heading>
+
+            <Flex alignContent="center" alignItems="center" mt="5%" flexFlow="column">
+
+               <Box p="3%" position="relative">
+                   <Box position="absolute" height="100%" bgColor="blue.400" borderRadius="15" opacity="0.7" top="0" left="0" w="100%" zIndex="1"/>
+
+                   <Box position="relative" zIndex="2">
+                       <Icon as={IoSchool} w={20} h={20} color="black" zIndex="inherit"/>
+                       <Text fontSize="4xl" color="white" zIndex="inherit">Southern New Hampshire University</Text>
+                       <Text fontSize="4xl" color="white" zIndex="inherit">BS Computer Science</Text>
+                       <Text fontSize="4xl" color="white" zIndex="inherit">June 2019 - Current...</Text>
+                   </Box>
+               </Box>
+
+            </Flex>
 
         </Box>
 
