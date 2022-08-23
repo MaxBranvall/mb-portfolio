f499ad286d04ada6d869a4f69765df73944677d0
Maxwell J Branvall <maxbranvall@gmail.com>
2022-08-23 00:48:38 -0400

    - Began working on projects page.

--- src/components/ProjectsPage/main.js
+++ src/components/ProjectsPage/main.js
@@ -1,5 +1,5 @@
 
-import { Box, Heading, Text, Button, PopoverHeader } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, PopoverHeader, Flex, Link } from "@chakra-ui/react";
 import { Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody } from "@chakra-ui/react";
 
 function ProjectsPage() {
@@ -10,20 +10,66 @@ function ProjectsPage() {
 
             <Heading variant="mainHeader">Projects</Heading>
 
+            <Flex>
+                <Box p="3%" position="relative">
+                    <Box position="absolute" height="100%" bgColor="blue.400" borderRadius="15" opacity="0.7" top="0" left="0" w="100%"/>
+                    <Box position="relative" zIndex="2">
+                        <Text>WinMixerDeck</Text>
+                        <Link>Repo</Link>
+                        <Text>Description</Text>
+                        <Text>Technologies Used</Text>
+                    </Box>
+                </Box>
+            </Flex>
 
-            <Text color="white">GTA-Cursor, Momscookbook, WinMixerDeck, try and finish autodirectory, maybe build maintnence minder site</Text>
+            {/* <Text color="white">GTA-Cursor, Momscookbook, WinMixerDeck, try and finish autodirectory, maybe build maintnence minder site</Text> */}
 
-            <Popover>
-                <PopoverTrigger>
-                    <Button>Project One</Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <PopoverArrow />
-                    <PopoverCloseButton />
-                    <PopoverHeader>The header</PopoverHeader>
-                    <PopoverBody>Body of popover</PopoverBody>
-                </PopoverContent>
-            </Popover>
+            {/* <Flex justifyContent="space-around">
+                <Popover>
+                    <PopoverTrigger>
+                        <Button>WinMixerDeck</Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>The header</PopoverHeader>
+                        <PopoverBody>Body of popover</PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                <Popover>
+                    <PopoverTrigger>
+                        <Button>Trigger</Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Header</PopoverHeader>
+                        <PopoverBody>Body of popover</PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                <Popover>
+                    <PopoverTrigger>
+                        <Button>Trigger</Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Header</PopoverHeader>
+                        <PopoverBody>Body of popover</PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                <Popover>
+                    <PopoverTrigger>
+                        <Button>Trigger</Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Header</PopoverHeader>
+                        <PopoverBody>Body of popover</PopoverBody>
+                    </PopoverContent>
+                </Popover>
+            </Flex> */}
 
         </Box>
 
