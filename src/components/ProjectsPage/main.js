73470def2ffd92e67618af9fed4681a8d2e20593
Max Branvall <maxbranvall@gmail.com>
2022-08-24 03:56:54 -0400

    - Implemented loop rendering to projects page     - Added a couple mock projects for testing - Began implementing responsive display on projects page - Still need to refine styles

--- src/components/ProjectsPage/main.js
+++ src/components/ProjectsPage/main.js
@@ -1,8 +1,62 @@
 
-import { Box, Heading, Text, Button, PopoverHeader, Flex, Link } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, PopoverHeader, Flex, Link, HStack, Icon, Spacer } from "@chakra-ui/react";
 import { Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody } from "@chakra-ui/react";
+import { SiCsharp, SiCss3, SiHtml5, SiJavascript } from "react-icons/si"
 
 function ProjectsPage() {
+
+    const responsiveText = {base: "xl", md: "2xl", xl: "4xl"}
+    const responsiveDisplay = {base: "none", md: "flex"}
+    const responsiveDisplay2 = {base: "flex", md: "none", xl: "none"}
+    const iconSize = "40px";
+
+    var projects = []
+    var projectJSX = []
+
+    let winMixerDeck = {
+        title: "WinMixerDeck",
+        repoLink: "https://github.com/MaxBranvall/WinMixerDeck",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium purus sed semper feugiat. Ut purus magna, ultrices interdum elementum non, pretium at orci. Vivamus aliquet molestie risus, ut feugiat ante laoreet eu. Maecenas at nunc at urna pretium accumsan. Morbi aliquet feugiat ipsum, eu hendrerit lacus commodo eget. Praesent fermentum vestibulum ante, in auctor mi semper nec. Ut lectus libero, porttitor ac lectus non, vestibulum tincidunt ante. Nam tempor scelerisque quam. Fusce consequat ultrices ante, ut dignissim lacus sagittis auctor. Nunc et rhoncus mauris.",
+        technologiesUsed: [
+            <Icon h={iconSize} w={iconSize} color="violet" as={SiCsharp} />,
+            <Icon h={iconSize} w={iconSize} color="blue.300" as={SiCss3} />,
+            <Icon h={iconSize} w={iconSize} color="orange.500" as={SiHtml5} />,
+            <Icon h={iconSize} w={iconSize} color="yellow" as={SiJavascript} />
+        ]
+    }
+
+    let gtaCursor = {
+        title: "GTACursor",
+        repoLink: "https://github.com/MaxBranvall/GTAStyleCursor",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium purus sed semper feugiat. Ut purus magna, ultrices interdum elementum non, pretium at orci. Vivamus aliquet molestie risus, ut feugiat ante laoreet eu. Maecenas at nunc at urna pretium accumsan. Morbi aliquet feugiat ipsum, eu hendrerit lacus commodo eget. Praesent fermentum vestibulum ante, in auctor mi semper nec. Ut lectus libero, porttitor ac lectus non, vestibulum tincidunt ante. Nam tempor scelerisque quam. Fusce consequat ultrices ante, ut dignissim lacus sagittis auctor. Nunc et rhoncus mauris.",
+        technologiesUsed: [
+            <Icon h={iconSize} w={iconSize} color="violet" as={SiCsharp} />
+        ]
+    }
+
+    projects.push(winMixerDeck);
+    projects.push(gtaCursor);
+
+    projects.forEach(el => {
+        projectJSX.push(
+            <Box p="1%" position="relative" maxWidth="20%">
+                <Box position="absolute" height="100%" bgColor="blue.400" borderRadius="15" opacity="0.7" top="0" left="0" w="100%"/>
+                <Box position="relative" zIndex="2" color="white" h="100%">
+                    <Heading>{el.title}</Heading>
+                    <Link href={el.repoLink} textDecoration="underline" fontSize={responsiveText}>Link to repo</Link>
+                    <Text>
+                        {el.description}
+                    </Text>
+                    <Heading>Technologies Used</Heading>
+                    <HStack justifyContent="center" mt="3%">
+                        {el.technologiesUsed}
+                    </HStack>
+                </Box>
+            </Box>
+        )
+    })
+
+    let i = 0;
 
     return (
 
@@ -10,21 +64,13 @@ function ProjectsPage() {
 
             <Heading variant="mainHeader">Projects</Heading>
 
-            <Flex>
-                <Box p="3%" position="relative">
-                    <Box position="absolute" height="100%" bgColor="blue.400" borderRadius="15" opacity="0.7" top="0" left="0" w="100%"/>
-                    <Box position="relative" zIndex="2">
-                        <Text>WinMixerDeck</Text>
-                        <Link>Repo</Link>
-                        <Text>Description</Text>
-                        <Text>Technologies Used</Text>
-                    </Box>
-                </Box>
+            <Flex justifyContent="center" gridGap="10%" display={responsiveDisplay}>
+                {projectJSX}
             </Flex>
 
             {/* <Text color="white">GTA-Cursor, Momscookbook, WinMixerDeck, try and finish autodirectory, maybe build maintnence minder site</Text> */}
 
-            {/* <Flex justifyContent="space-around">
+            <Flex justifyContent="space-around" display={responsiveDisplay2}>
                 <Popover>
                     <PopoverTrigger>
                         <Button>WinMixerDeck</Button>
@@ -69,7 +115,7 @@ function ProjectsPage() {
                         <PopoverBody>Body of popover</PopoverBody>
                     </PopoverContent>
                 </Popover>
-            </Flex> */}
+            </Flex>
 
         </Box>
 
