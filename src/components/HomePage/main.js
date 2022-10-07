aadcdaae7ed50537e0f7e983f72b7f793239385e
Maxwell J Branvall <maxbranvall@gmail.com>
2022-10-07 00:55:38 -0400

    - Updated linkedin URL

--- src/components/HomePage/main.js
+++ src/components/HomePage/main.js
@@ -36,7 +36,7 @@ function HomePage() {
                 <HStack justifyContent="center" spacing="1em" fontSize="xl" zIndex="inherit" mt={2}>
                     <Link color="white" href="mailto: maxbranvall@gmail.com"><EmailIcon boxSize="8" /></Link>
                     <Link color="white" href="https://github.com/MaxBranvall" isExternal><Icon as={AiOutlineGithub} boxSize="8" /></Link>
-                    <Link color="white" href="https://www.linkedin.com/in/max-branvall-980272247/" isExternal><Icon as={AiFillLinkedin} boxSize="8" /></Link>
+                    <Link color="white" href="https://www.linkedin.com/in/maxbranvall/" isExternal><Icon as={AiFillLinkedin} boxSize="8" /></Link>
                 </HStack>
             </Flex>
         </Box>
